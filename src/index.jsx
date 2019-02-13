2229f5402bf4d27b68b25ff9a1bf0f0e9af3debe
Rasmus Jauri <rasmus.jauri@gmail.com>
2019-02-13 17:04:53 +0200

    add api fetch

--- src/index.jsx
+++ src/index.jsx
@@ -8,13 +8,29 @@ import './assets/stylesheets/style.css'
 /****** ADD YOUR CODE AFTER THIS LINE ******/
 
 const Hello = () => (
-  <div>Hello World!</div>
+  <div>Hello!</div>
 );
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      data: "tähän dataa"
+    }
+  }
+  componentDidMount(){
+    console.log("moi")
+  fetch("https://weatherapp.eficode.fi/api/forecast?lat=60.1698509&lon=24.9247536")
+   .then(result => result.json())
+   .then((data) => {
+    this.setState({data: JSON.stringify(data) })
+   })
+   .catch(error => console.log(error))
+  }
+
   render() {
     return (
-      <Hello />
+      this.state.data
     );
   }
 }
